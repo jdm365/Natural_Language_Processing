fdebc536257451b50b8620416fc2b6b3f2f4cc24
jdm365 <jdm365@georgetown.edu>
2022-05-01 14:37:13 -0400

    Pushing work. Need to finish preprocessing but am going to start on RL idea now.

--- transformer_models/bert.py
+++ transformer_models/bert.py
@@ -4,44 +4,80 @@ import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
 from transformer import Transformer
-import sentencepiece as spm
 
 class Preprocesser:
-    def __init__(self, filename, sentence_length, vocab_size, batch_size):
+    def __init__(self, filename, sentence_length, batch_size, mask_frequency, vocab_filename='words_alpha.txt'):
         self.filename = filename
-        self.vocab_size = vocab_size
-        self.word_list = self.generate_word_list()
-        self.tokenizer = self.create_tokenizer()
+        self.text = self.stringify_txt_file()
+        self.vocab_filename = vocab_filename
+        self.vocab, self.vocab_size = read_vocab_file()
+        self.word_map = self.create_word_map()
+        self.add_sep_tokens()
+        self.word_list = self.text.split(' ')
+        self.sentence_length = sentence_length
         self.batch_size = batch_size
+        self.mask_frequency = mask_frequency
 
 
-    def generate_word_list(self):
+    def stringify_txt_file(self):
         with open(self.filename, 'r') as file:
             data = file.read()
             data = data.lower()
-            return data.split()
+            return data
+
+    def read_vocab_file(self): 
+        with open(self.vocab_filename, 'r') as file:
+            data = file.read()
+            data = data.lower()
+            vocab = data.splitlines()
+            return vocab, len(vocab)
+
+    def add_sep_tokens(self):
+            self.text = self.text.replace('.', ' <sep>')
+            self.vocab.append('<sep>')
+            self.vocab_size += 1
+            self.word_map['<sep>'] = self.vocab_size
+
+    def create_word_map(self):
+        self.word_map = {}
+        for idx, word in self.vocab:
+            self.word_map[word] = idx
+        
+    def mask_inputs(self, sentences):
+        new_sentences = []
+        tot_idxs = []
+        for sentence in sentences:
+            new = sentence
+            n_masked = int(len(sentence) * self.mask_frequency)
+            masker = np.random.choice(3, n_masked, p=[0.8, 0.1, 0.1])
+            idxs = np.random.randint(1, len(new), n_masked)
+            for mask, idx in zip(masker, idxs):
+                if '<sep>'.isin(new[idx]):
+                    continue
+                elif mask == 0:
+                    new[idx] = '<mask>'
+                elif mask == 1:
+                    new[idx] = np.random.choice(self.vocab)
+                else:
+                    continue
+            new_sentences.append(new)
+            tot_idxs.append(idxs)
+        return new_sentences, sentences, tot_idxs
 
 
-    def create_tokenizer(self):
-        spm.SentencePieceTrainer.train(input=self.filename, model_prefix='m',\ 
-                                        user_defined_symbols=['<sep>','<cls>'],\
-                                        vocab_size=self.vocab_size)
-        sp = spm.SentencePieceProcessor()
-        sp.load('m.model')
-        return sp
-
-    def add_cls_sep_tokens(self):
-        return
-
-    
     def load_batch_of_strings(self):
         ## return dims (batch_size, list(sentence_length))
-        start_idxs = np.random.randint(0, len(self.word_list) - self.sentence_length, \
+        start_idxs = np.random.randint(0, self.vocab_size - self.sentence_length, \
                                       self.batch_size)
-        sentence_lists = [self.word_list[idx:idx+self.batch_size] for idx in start_idxs]
-        sentences = ' '.join([str(item) for item in sentence_lists])
-        tokenized = self.tokenizer.encode_as_ids(sentences)
-        return tokenized
+        sentence_lists = ['<cls>' + self.word_list[idx:idx+self.batch_size] for idx in start_idxs]
+        masked, unmasked, label_idxs  = self.mask_inputs(sentences)
+        ## TODO: Make word map indexer for list and list of lists.
+        masked_tokenized = self.word_map[masked]
+        unmasked_tokenized = self.tokenizer.encode_as_ids(unmasked)
+        return masked_tokenized, unmasked_tokenized
 
 
+if __name__ == '__main__':
+    preprocessor = Preprocesser(filename='datasetSentences.txt', sentence_length=512,
+                                vocab_size=5000, batch_size=64, mask_frequency=0.15)
 
